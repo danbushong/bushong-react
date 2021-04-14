1bb866a91c4549c6c43b018e128e282f82fdd061
Dan Bushong <bushongdan@gmail.com>
2021-04-14 12:44:09 -0500

    fun AI button added

--- src/pages/TalktoAI.js
+++ src/pages/TalktoAI.js
@@ -1,23 +1,41 @@
-import React from "react";
+import React, {useState} from "react";
 import Pageconstant from "../components/Pageconstant";
 import "../styles/TalktoAI.css";
 
 function TalktoAI() {
 
+    const [SecretMessage, setSecretMessage] = useState("Click the button and see what happens")
+
     
-    function CreateMessage() {
-        console.log("maybe")
+
+    
+    function handleClick() {
+        
+        setSecretMessage("Ouch, stop poking me! (I am not a real AI)")
 
         
     }
+    
 
 
     return (
 
         <div>
             <Pageconstant />
+
+            <div style={{marginTop: "10%", textAlign: "center"}} >
+
+            <button style={{ borderRadius: "25px", color: "white", backgroundColor: "#007bff" }} onClick={handleClick} >Am I an AI?</button>
+
+            <div style={{marginTop: "3%" }}>{SecretMessage}</div>
+
+
+
+            </div>
            
-            <button style={{marginTop: "10%", marginLeft: "50%"}} onClick={CreateMessage}>Am I an AI?</button>
+           
+
+            
                 
             
                 
