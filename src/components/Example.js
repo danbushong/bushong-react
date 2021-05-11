ddc1d3713cfcaf52b3421b9625a1ce696b687f2c
Dan Bushong <bushongdan@gmail.com>
2021-05-11 09:12:08 -0500

    starting new idea

---
+++ src/components/Example.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Example() {
+    return (
+        <div>
+            <div className="Container">
+                this is eventually going to hold something cool.
+            </div>
+            
+        </div>
+    )
+}
+
+export default Example
